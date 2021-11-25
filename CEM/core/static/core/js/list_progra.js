d3b9c1c8835a8cb474a1947e798bb8d2005672ed
felipediazz <f.diazse@duocuc.cl>
2021-11-25 01:00:14 -0300

    generador de reportes + traduccion+FAw+arreglo css

--- CEM/core/static/core/js/list_progra.js
+++ CEM/core/static/core/js/list_progra.js
@@ -1,5 +1,7 @@
 $(document).ready( function () {
     $('#datatable').DataTable({
+        dom: 'PBfrtip',
+
         language: {
 
             search:         "Buscar",
@@ -13,20 +15,64 @@ $(document).ready( function () {
                 first:      "Primero",
                 previous:    "Anterior",
                 next:       "Siguiente"
+            },
+            searchPanes: {
+                clearMessage: 'Borrar selecciones',
+                showMessage: 'Mostrar todos',
+                collapseMessage: 'Ocultar todos',
+                collapse: {0: 'Opciones de filtrado', _: 'Opciones de filtrado (%d)'},
+                title: {
+                    _: 'Filtros Seleccionados - %d',
+                    0: 'Ningun filtro seleccionado',
+                    1: 'Un filtro seleccionado'
+                },
+            
             }
             },
-
+        responsive: "true",
             
         searchPanes:{
-            
+            cascadePanes: true,
+            viewTotal: true,
             dtOpts:{
                 dom:'tp',
-                searching:false
-                
+                searching:false     
             }
           },
-          dom: 'Pfrtip',
-          columnDefs: [
+        buttons:[ 
+			{
+                extend:    'excelHtml5',
+                text:      '<i class="fas fa-file-excel"></i> ',
+                titleAttr: 'Exportar a Excel',
+                className: 'btn btn-success'
+            },
+            {
+                extend:    'pdfHtml5',
+                text:      '<i class="fas fa-file-pdf"></i> ',
+                titleAttr: 'Exportar a PDF',
+                className: 'btn btn-danger'
+            },
+            {
+                extend:    'print',
+                text:      '<i class="fa fa-print"></i> ',
+                titleAttr: 'Imprimir',
+                className: 'btn btn-info'
+            },
+            {
+                extend:    'copy',
+                text:      '<i class="far fa-copy"></i>',
+                titleAttr: 'Copiar',
+                className: 'btn btn-primary'
+            },
+            {
+                extend:    'csv',
+                text:      '<i class="fas fa-file-csv"></i>',
+                titleAttr: 'Exportar CSV',
+                className: 'btn btn-warning'
+            },
+            
+		],		
+        columnDefs: [
           {
               searchPanes: {
                   show: true,
@@ -37,3 +83,84 @@ $(document).ready( function () {
     
   });
 } );
+
+
+/*$(document).ready( function () {
+    $('#datatable').DataTable({
+        dom: 'Bfrtip',
+        serverSide: false,
+        stateSave: false,
+        language: {
+
+            search:         "Buscar",
+            infoEmpty: "Sin resultado",
+            zeroRecords: "Sin resultados encontrados",
+            info: "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+            infoEmpty: "Mostrando 0 to 0 of 0 Entradas",
+            infoFiltered: "(Filtrado de _MAX_ total entradas)",
+            infoPostFix: "",
+            paginate: {
+                first:      "Primero",
+                previous:    "Anterior",
+                next:       "Siguiente"
+            },
+            searchPanes: {
+                clearMessage: 'Borrar selecciones',
+                showMessage: 'Mostrar todos',
+                collapseMessage: 'Ocultar todos',
+                collapse: {0: 'Opciones de filtrado', _: 'Opciones de filtrado (%d)'},
+                title: {
+                    _: 'Filtros Seleccionados - %d',
+                    0: 'Ningun filtro seleccionado',
+                    1: 'Un filtro seleccionado'
+                }
+            }
+            },
+            buttons: [
+                {
+                    extend: "searchPanes",
+                    
+                    text: '<i class="fas fa-file-csv"></i>',
+                    titleAttr: 'Paneles de busqueda',
+                    config: {
+                        layout: "columns-2",
+                        cascadePanes: true
+                        
+                    },
+                    className: 'btn btn-dark'
+                },
+                {
+                    extend:    'excelHtml5',
+                    text:      '<i class="fas fa-file-excel"></i> ',
+                    titleAttr: 'Exportar a Excel',
+                    className: 'btn btn-success'
+                },
+                {
+                    extend:    'pdfHtml5',
+                    text:      '<i class="fas fa-file-pdf"></i> ',
+                    titleAttr: 'Exportar a PDF',
+                    className: 'btn btn-danger'
+                },
+                {
+                    extend:    'print',
+                    text:      '<i class="fa fa-print"></i> ',
+                    titleAttr: 'Imprimir',
+                    className: 'btn btn-info'
+                },
+                {
+                    extend:    'copy',
+                    text:      '<i class="far fa-copy"></i>',
+                    titleAttr: 'Copiar',
+                    className: 'btn btn-primary'
+                },
+                {
+                    extend:    'csv',
+                    text:      '<i class="fas fa-file-csv"></i>',
+                    titleAttr: 'Exportar CSV',
+                    className: 'btn btn-warning'
+                },
+            ]
+    
+  });
+} );
+*/
